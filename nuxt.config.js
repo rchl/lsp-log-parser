1a89eead7c7150452f4dd428fab14a80c03c92be
Rafał Chłodnicki <rchl2k@gmail.com>
2020-04-28 23:17:22 +0200

    Update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,6 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
