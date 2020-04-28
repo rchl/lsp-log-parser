3ef7fd7e78d2f1a2350a56ac11abc7be4cea3438
Rafał Chłodnicki <rchl2k@gmail.com>
2020-04-28 23:18:29 +0200

    Human friendly title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'LSP Log Parser',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
