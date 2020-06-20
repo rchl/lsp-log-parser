0333ae941027a741cf5dcccc7cd28309ebb79c92
Rafał Chłodnicki <rchl2k@gmail.com>
2020-06-20 10:07:39 +0200

    Update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ const configuration = {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
   /*
