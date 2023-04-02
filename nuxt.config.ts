27f3b875d4e2e49d3a3f46dca80007865f31a52a
Rafal Chlodnicki <rchl2k@gmail.com>
2023-04-02 16:38:27 +0200

    update nuxt to 3.3.3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,44 +13,37 @@ export default defineNuxtConfig({
     },
     build: {
         transpile: ['vuetify'],
-        // transpile: [
-        //     'vscode-jsonrpc',
-        //     'vscode-languageserver-protocol',
-        // ],
     },
     vite: {
       define: {
         'process.env.DEBUG': false,
       },
     },
-    head: {
-        title: 'LSP Log Parser',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
-        ],
-    },
-    loading: { color: '#fff' },
-    loadingIndicator: {
-        name: 'pulse',
-        color: '#3B8070',
-        background: 'white',
-    },
+    // head: {
+    //     title: 'LSP Log Parser',
+    //     meta: [
+    //         { charset: 'utf-8' },
+    //         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    //         { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+    //     ],
+    //     link: [
+    //         { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+    //     ],
+    // },
+    // loading: { color: '#fff' },
+    // loadingIndicator: {
+    //     name: 'pulse',
+    //     color: '#3B8070',
+    //     background: 'white',
+    // },
     googleFonts: {
         download: true,
         families: {
             Roboto: [300, 400, 500],
         },
     },
-    buildModules: [
-        '@nuxtjs/stylelint-module',
-        '@nuxtjs/vuetify',
-    ],
     modules: [
+        '@nuxtjs/stylelint-module',
         '@nuxtjs/google-fonts',
         '@vueuse/nuxt',
         (options, nuxt) => {
@@ -63,8 +56,6 @@ export default defineNuxtConfig({
     stylelint: {
         context: resolve(__dirname),
         lintDirtyModulesOnly: true,
-    },
-    vuetify: {
-        customVariables: ['~/assets/variables.scss'],
+        lintOnStart: false,
     },
 })
